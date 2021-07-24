70da7bf0bf4fd91b79ad423e8715a927de4a4f06
sanzhixiong1986 <57082213+sanzhixiong1986@users.noreply.github.com>
2021-07-24 16:04:50 +0800

    第八天修改

---
+++ day08/classMVC_8/UICtrlGenTools/Temp_Ctrl.ts
@@ -0,0 +1,11 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+import { UICtrl } from "./../../Framework/Managers/UICtrl";
+
+@ccclass('Temp_Ctrl')
+export class Temp_Ctrl extends UICtrl {
+    onLoad(): void {
+        super.onLoad();
+    }
+}
